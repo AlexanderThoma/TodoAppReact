fd3dfb6cbe9fae3657f0c4b25a4a39d531148cbc
Alexander Thoma <thomalexander92@outlook.com>
2024-06-02 13:57:21 +0200

    feat: stuff

--- src/backend/Program.cs
+++ src/backend/Program.cs
@@ -4,8 +4,24 @@ const string myAllowSpecificOrigins = "disable cors";
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+/*builder.Services
+    .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)  
+    .AddMicrosoftIdentityWebApi(builder.Configuration.GetSection("AzureAd"));
+builder.Services.AddRequiredScopeAuthorization();
+
+builder.Services.AddAuthorizationBuilder()
+    .AddDefaultPolicy("access_as_user_policy", policy =>
+    {
+        // Validate if the scope 'access_as_user' was added  
+        policy.RequireScope("access_as_user");
+
+        // Validate id of application for which the token was created  
+        // The client app (frontend app) id should be the same as the one in the token
+        // https://learn.microsoft.com/en-us/entra/identity-platform/access-token-claims-reference#payload-claims
+        policy.RequireClaim("azp", builder.Configuration["Policy:ClientId"]!);
+    });*/
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddHttpContextAccessor();
@@ -25,11 +41,8 @@ builder.Services.AddCors(options =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseCors(myAllowSpecificOrigins);
 
